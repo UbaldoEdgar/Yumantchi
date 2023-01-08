f6add2df9601eeeb610dfd1572c908c7cec3973f
Edgar Ubaldo <fakeemail@email.com>
2023-01-07 17:39:44 -0800

    fix linter warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
       "error",
       { vars: "all", args: "after-used", ignoreRestSiblings: false },
     ],
-    "@typescript-eslint/explicit-function-return-type": "warn", // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+    //"@typescript-eslint/explicit-function-return-type": "warn", // Consider using explicit annotations for object literals and function return types even when they can be inferred.
     "no-empty": "warn",
   },
 };
